7b0b976eb2440d23b8b5b8dd67ead9fc4d1a6ef6
Jirakit <jw.jirakit@gmail.com>
2020-02-08 22:15:52 +0700

    Adjust comment

--- 01_variables/01_code.py
+++ 01_variables/01_code.py
@@ -1,5 +1,11 @@
+# string1 is type string variable with value is "Jame"
 string1 = "Jame"
+
+# string2 is type string variable with value is "Jirakit"
 string2 = "Jirakit"
 
+# string2 is type integer variable with value is "1993"
 num1 = 1993
+
+# string2 is type integer variable with value is "2020"
 num2 = 2020
