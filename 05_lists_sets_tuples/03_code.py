ef423634c68bf7497e8c6bd03e4f73e3731b57e9
Jirakit <jw.jirakit@gmail.com>
2020-02-23 13:10:17 +0700

    Example lists, sets and tuples

---
+++ 05_lists_sets_tuples/03_code.py
@@ -0,0 +1,28 @@
+# lists
+l = ['Apple', 'Banana', 'Mango']
+
+# Add en element to the end of the list
+l.append('Pineapple')
+print(l)
+
+# Remove en element from the list by value
+l.remove('Banana')
+print(l)
+
+l[0] = 'Orange'
+print(l)
+
+
+# Sets
+s = {'Apple', 'Banana', 'Mango'}
+
+# Add en element to the list 
+s.add('Orange')
+print(s)
+
+
+# Tuples
+t = ('Apple', 'Banana', 'Mango')
+# tuples is not allow modify elements
+
+print(t)
